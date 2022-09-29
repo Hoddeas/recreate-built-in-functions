7170e5cb8c206d278e7bf112db80009ae7a7d972
Hoddeas <fakehoddeas@gmail.com>
2022-09-29 11:25:06 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,12 @@
 // DEFINE YOUR FUNCTIONS HERE BASED ON THE PROVIDED CRITERIA
+
+// array.includes()
+function includes(item, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == item) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
